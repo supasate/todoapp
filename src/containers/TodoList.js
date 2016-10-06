a2b1b0ca05619954d43cc92fc3a8f3bfda810b59
Supasate Choochaisri <supasate.c@gmail.com>
2016-10-06 17:54:58 +0700

    Map dispatch to props of toggleTodo

--- src/containers/TodoList.js
+++ src/containers/TodoList.js
@@ -1,8 +1,13 @@
 import { connect } from 'react-redux'
 import TodoList from '../components/TodoList'
+import { toggleTodo } from '../actions/todo'
 
 const mapStateToProps = state => ({
   todos: state.todos,
 })
 
-export default connect(mapStateToProps)(TodoList)
+const mapDispatchToProps = dispatch => ({
+  toggleStatus: text => dispatch(toggleTodo(text)),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList)
