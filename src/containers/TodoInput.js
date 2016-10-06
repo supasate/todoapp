1f45c895b9893e7b2af31a74a97abaced91768b3
Supasate Choochaisri <supasate.c@gmail.com>
2016-10-06 17:16:49 +0700

    Refactor to use shorthand mapDispatchToProps

--- src/containers/TodoInput.js
+++ src/containers/TodoInput.js
@@ -2,8 +2,8 @@ import { connect } from 'react-redux'
 import TodoInput from '../components/TodoInput'
 import { addTodo } from '../actions/todo'
 
-const mapDispatchToProps = dispatch => ({
+/* const mapDispatchToProps = dispatch => ({
   addTodo: text => dispatch(addTodo(text)),
-})
+}) */
 
-export default connect(null, mapDispatchToProps)(TodoInput)
+export default connect(null, { addTodo })(TodoInput)
