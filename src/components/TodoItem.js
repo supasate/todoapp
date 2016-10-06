8230a14ee03ddebd3468715998817fb3f0f5b256
Supasate Choochaisri <supasate.c@gmail.com>
2016-10-06 16:33:30 +0700

    Fix lint of TodoItem

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,11 +1,12 @@
 import React, { PropTypes } from 'react'
 
-const TodoItem = ({text, complete, toggleStatus}) => (
-  <li onClick={ () => { toggleStatus(text) } } className={complete?'complete':''}>{text}</li>
+const TodoItem = ({ text, complete, toggleStatus }) => (
+  <li onClick={() => { toggleStatus(text) }} className={complete ? 'complete' : ''}>{text}</li> // eslint-disable-line jsx-a11y/no-static-element-interactions
 )
 
 TodoItem.propTypes = {
   complete: PropTypes.bool.isRequired,
   text: PropTypes.string.isRequired,
+  toggleStatus: PropTypes.func.isRequired,
 }
 export default TodoItem
