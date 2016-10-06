7086715b33133585af53921f3a65c5a4f8066f5d
Supasate Choochaisri <supasate.c@gmail.com>
2016-10-06 17:10:53 +0700

    todoReducer handles ADD_TODO action

--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -1,7 +1,18 @@
+import { ADD_TODO } from '../actions/types'
+
 const initialState = []
 
 const todoReducer = (state = initialState, action) => {
   switch (action.type) {
+    case ADD_TODO: {
+      const newTodo = {
+        text: action.payload.text,
+      }
+      return [
+        newTodo,
+        ...state,
+      ]
+    }
     default: {
       return state
     }
