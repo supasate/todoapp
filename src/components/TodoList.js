c6d2bbc0f6b5522e1aab79fee7729a87ba75a09a
Supasate Choochaisri <supasate.c@gmail.com>
2016-10-06 16:36:06 +0700

    Fix lint for TodoList

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,27 +1,26 @@
 import React, { PropTypes } from 'react'
 import TodoItem from './TodoItem'
 
-const TodoList = ({todos, toggleStatus}) => (
+const TodoList = ({ todos, toggleStatus }) => (
   <ul>
-    { todos.map((todo) => {
-        return (
-          <TodoItem
-            key={todo.text}
-            text={todo.text}
-            complete={todo.complete}
-            toggleStatus={toggleStatus}
-          />
-        )
-    })}
+    { todos.map(todo => (
+      <TodoItem
+        key={todo.text}
+        text={todo.text}
+        complete={todo.complete}
+        toggleStatus={toggleStatus}
+      />
+    ))}
   </ul>
 )
 
 TodoList.propTypes = {
-  todos: PropTypes.array.isRequired
+  todos: PropTypes.arrayOf(PropTypes.object).isRequired,
+  toggleStatus: PropTypes.func.isRequired,
 }
 
 TodoList.defaultProps = {
-  todos: []
+  todos: [],
 }
 
 export default TodoList
