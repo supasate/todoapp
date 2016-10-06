35764ab8a7e6e495d7d9ea201b528c18eb8d89e1
Supasate Choochaisri <supasate.c@gmail.com>
2016-10-06 16:26:29 +0700

    Refactor TodoApp to be stateless functional component

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,51 +1,11 @@
-import React, { Component } from 'react'
+import React from 'react'
 import TodoInput from './TodoInput'
 import TodoList from './TodoList'
 
-class TodoApp extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      todos:[
-        { text: "read book", complete: true},
-        { text: "go for walk", complete: false},
-      ]
-    }
-    this.addTodo = this.addTodo.bind(this)
-    this.toggleStatus = this.toggleStatus.bind(this)
-  }
-  addTodo(text) {
-    this.setState({
-      todos: [
-        ...this.state.todos,
-        {
-          text: text,
-          complete: false,
-        }
-      ],
-    })
-  }
-  toggleStatus(text) {
-    console.log(text)
-    this.setState({
-      todos: this.state.todos.map((todo) => {
-        return (todo.text==text)
-          ?{ text: todo.text, complete: !todo.complete }
-          :todo
-      })
-    })
-  }
-  render() {
-    return (
-      <div>
-        <TodoInput addTodo={this.addTodo} />
-        <TodoList 
-          todos={this.state.todos}
-          toggleStatus={this.toggleStatus}
-        />
-      </div>
-    )
-  }
-}
-
+const TodoApp = () => (
+  <div>
+    <TodoInput />
+    <TodoList />
+  </div>
+)
 export default TodoApp
