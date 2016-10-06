6ce41d44ac1cb769abb19d5957eae1b4fbc83691
Supasate Choochaisri <supasate.c@gmail.com>
2016-10-06 16:49:06 +0700

    Create store and pass down with Provider

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import TodoApp from './components/TodoApp'
-import styles from './styles.css'
+import rootReducer from './reducers'
 
-ReactDOM.render(<TodoApp />,
+const store = createStore(rootReducer)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <TodoApp />
+  </Provider>,
   document.getElementById('react-root'))
